e382048053587583d0b5d28189c3bb2d7730a943
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-06-30 22:19:34 -0400

    Updated the server to point at the right controller

--- config/orm.js
+++ config/orm.js
@@ -1,46 +1,74 @@
-// Import MySQL connection.
-var connection = require("../config/connection.js");
 
-// Export the orm object for the model (burger.js).
-module.exports = function(table) {
-  // Return an object for all our SQL statement functions for the specified table.
-  return {
-    findAll: function(cb) {
-      connection.query("SELECT * FROM ??", [table], function(err, result) {
-        if (err) throw err;
 
-        cb(result);
-      });
-    },
-    // condition in object form: { id: 1 }
-    findBy: function(condition, cb) {
-      connection.query("SELECT * FROM ?? WHERE ?", [condition], function(err, result) {
-        if (err) throw err;
+var connection = require("./connection.js");
 
-        cb(result);
-      })
-    },
-    create: function(newRow, cb) {
+function printQuestionMarks(num) {
+  var arr = [];
 
-      connection.query("INSERT INTO ?? SET ?", [table, newRow], function(err, result) {
-        if (err) throw err;
-        
-        cb(result);
-      });
-    },
-    update: function(updatedObj, condition, cb) {
-      connection.query("UPDATE ?? SET ? WHERE ?", [table, updatedObj, condition], function(err, result) {
-        if (err) throw err;
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
 
-        cb(result);
-      });
-    },
-    delete: function(condition, cb) {
-      connection.query("DELETE FROM ?? WHERE ?", [table, condition], function(err, result) {
-        if (err) throw err;
-
-        cb(result);
-      });
-    }
-  } 
+  return arr.toString();
 }
+
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    arr.push(key + "=" + ob[key]);
+  }
+
+  return arr.toString();
+}
+
+var orm = {
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+ 
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
